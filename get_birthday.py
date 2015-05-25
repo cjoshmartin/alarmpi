3d648b260fcbd50f503def3608986bf240ac3af0
cjoshmartin <joshmartin5001@gmail.com>
2015-05-25 03:36:56 -0400

    s

--- get_birthday.py
+++ get_birthday.py
@@ -25,7 +25,7 @@ print birthday
 if birthday == 'null':
   birthday = ''
 else:
-  birthday = 'God!... ' + birthday + 'Your getting old!...  ' 
+  birthday = 'God!... ' + birthday + 'It\'s your Birthday...Your getting old!...  ' 
 
 if Config.get('main','debug') == str(1):
   print birthday
